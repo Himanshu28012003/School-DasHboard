1a434927f8ca1d5d569ce074c2b626dd1273d38e
singhabhinav29 <2801.himanshu@gmail.com>
2026-03-15 12:13:05 +0530

    changes

--- Frontend/school/src/constants/index.ts
+++ Frontend/school/src/constants/index.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = "https://school-dashboard-r4rh.onrender.com/api";
 export const AUTH_USERS_KEY = "school_auth_users";
 export const AUTH_CURRENT_USER_KEY = "school_auth_current_user";
+export const AUTH_TOKEN_KEY = "school_auth_token";
